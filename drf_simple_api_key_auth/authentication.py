1d50bd6e6df36126711a85524acbfeae032a32f5
Jacob Haslehurst <jacob@haslehurst.net>
2015-07-21 15:56:50 +1000

    initial implementation of SimpleApiKeyAuthentication

---
+++ drf_simple_api_key_auth/authentication.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+from . import settings
+
+
+class SimpleApiKeyAuthentication(TokenAuthentication):
+    """
+    Simple token based authentication. This should only be used by internal web
+    services. Clients should authenticate by passing the token key in the
+    "Authorization" HTTP header, prepended with the string "Key ".
+    For example:
+        Authorization: Key 401f7ac837da42b97f613d789819ff93537bee6a
+    """
+
+    def _get_user(self):
+        UserModel = get_user_model()
+
+        user, created = UserModel.get_or_create(
+            username=settings.USERNAME,
+            email=settings.EMAIL,
+            is_superuser=True,
+        )
+        
+        if created:
+            user.set_unusable_password()
+            user.save()
+
+        return user, key
+
+    def authenticate_credentials(self, key):
+        if key not in settings.API_KEYS:
+            raise AuthenticationFailed('Invalid key')
+        
+        user = self._get_user()
+
+    def authenticate_header(self, request):
+        return 'Key'
---
+++ drf_simple_api_key_auth/settings.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+USERNAME = getattr('REST_FRAMEWORK_SIMPLE_AUTH_USER_USERNAME', 'simple_api_user')
+EMAIL = getattr('REST_FRAMEWORK_SIMPLE_AUTH_USER_EMAIL', 'simple_api_user@example.com')
+
+try:
+    API_KEYS = settings.REST_FRAMEWORK_SIMPLE_AUTH_KEYS
+except AttributeError:
+    raise ImproperlyConfigured('REST_FRAMEWORK_SIMPLE_AUTH_KEYS not defined in django settings')
