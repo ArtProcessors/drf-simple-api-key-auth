bd8eb97662a4bb6d89659505d18addc8a8b26c53
Jacob Haslehurst <jacob@haslehurst.net>
2016-07-14 16:35:39 +1000

    refactor username/email settings to instead be defined as USER_KWARGS
    This allows a user of this app to support custom user models eg a model
    where the email address is used as the username

--- drf_simple_api_key_auth/authentication.py
+++ drf_simple_api_key_auth/authentication.py
@@ -18,11 +18,7 @@ class SimpleApiKeyAuthentication(TokenAuthentication):
     def _get_user(self):
         UserModel = get_user_model()
 
-        user, created = UserModel.objects.get_or_create(
-            username=settings.USERNAME,
-            email=settings.EMAIL,
-            is_superuser=True,
-        )
+        user, created = UserModel.objects.get_or_create(**settings.USER_KWARGS)
         
         if created:
             user.set_unusable_password()
--- drf_simple_api_key_auth/settings.py
+++ drf_simple_api_key_auth/settings.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-USERNAME = getattr(settings, 'REST_FRAMEWORK_SIMPLE_AUTH_USER_USERNAME', 'simple_api_user')
-EMAIL = getattr(settings, 'REST_FRAMEWORK_SIMPLE_AUTH_USER_EMAIL', 'simple_api_user@example.com')
+USER_KWARGS = getattr(settings, 'REST_FRAMEWORK_SIMPLE_AUTH_USER_KWARGS', {
+    'username': 'simple_api_user',
+    'email': 'simple_api_user@example.com',
+    'is_superuser': True,
+})
 
 try:
     API_KEYS = settings.REST_FRAMEWORK_SIMPLE_AUTH_KEYS
